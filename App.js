a696bf2c39ee55f0aab3e48d75d8aba96276401d
sunshinemoua <sunshinemoua21@gmail.com>
2024-02-02 14:02:40 -0800

    added google api key and enables google apis

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
 import { StyleSheet, Text, View, AppRegistry } from "react-native";
 import { Provider } from "react-redux";
 import store from "./store";
+import HomeScreen from "./screens/HomeScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import MapScreen from "./screens/MapScreen";
+import EatsScreen from "./screens/EatsScreen";
 
 const App = () => {
+  const Stack = createStackNavigator();
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Hello world</Text>
-      </View>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="EatsScreen"
+              component={EatsScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+
+          {/* <HomeScreen /> */}
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 };
---
+++ components/NavOptions.js
@@ -0,0 +1,54 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { Icon } from "react-native-elements";
+import tw from "tailwind-react-native-classnames";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
+
+const NavOptions = () => {
+  const navigation = useNavigation();
+
+  return (
+    <FlatList
+      data={data}
+      keyExtractore={(item) => item.id}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 bg-gray-200 m-2 w-40 h-60`}
+        >
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{ uri: item.image }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright"
+              color="white"
+              type="antdesign"
+            ></Icon>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
