52d3bdac29a0ca8e3580d398983912a73db84373
sunshinemoua <sunshinemoua21@gmail.com>
2024-02-08 14:51:29 -0800

    added google map and coordinate pin

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "tailwind-react-native-classnames";
+import Map from "../components/Map";
 
 const MapScreen = () => {
   return (
     <SafeAreaView>
       <Text>MapScreen</Text>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+      <View style={tw`h-1/2`}></View>
     </SafeAreaView>
   );
 };
