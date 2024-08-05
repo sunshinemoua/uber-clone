f6420a643e7bca3d9b1586049396c7444bea2f9e
sunshinemoua <sunshinemoua21@gmail.com>
2024-08-05 12:50:27 -0700

    fixed styling for save-everyday second carousel; fixed extra margin spacing above bottom nav

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import tw from "tailwind-react-native-classnames";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import RidesScreen from "./RidesScreen";
 import EatsScreen from "./EatsScreen";
